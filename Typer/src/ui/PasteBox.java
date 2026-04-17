34ec6bd72d9709773a432b17a57c7751bec82ab0
KM-0O7 <153666541+KM-0O7@users.noreply.github.com>
2026-04-17 13:56:34 -0400

    added JavaFX

---
+++ Typer/src/ui/PasteBox.java
@@ -0,0 +1,21 @@
+package ui;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class PasteBox extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hello JavaFX!");
+        Scene scene = new Scene(label, 400, 200);
+
+        stage.setTitle("JavaFX Test");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
