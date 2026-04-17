f4a3c3ba8dd6053662a0d87d9d9516ebd004c970
KM-0O7 <153666541+KM-0O7@users.noreply.github.com>
2026-04-17 17:33:33 -0400

    changed bat file

--- Typer/run.bat
+++ Typer/run.bat
@@ -1,3 +1,4 @@
 @echo off
-java --enable-native-access=javafx.graphics --module-path "%~dp0\javafx-sdk-26\lib" --add-modules javafx.controls,javafx.fxml -jar "%~dp0\Typing-MACHINE.jar"
+
+java --module-path "%~dp0\javafx-sdk-26\lib" --add-modules javafx.controls,javafx.fxml -jar "%~dp0\Typing-MACHINE.jar"
 pause
